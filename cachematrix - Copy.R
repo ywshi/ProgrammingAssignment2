3903e1873e4fd72b4a3eccea9ae5c5258f38bbf8
ywshi <yshi@miis.edu>
2015-01-24 09:59:31 -0800

    Assignment 2 file

--- cachematrix - Copy.R
+++ cachematrix - Copy.R
@@ -11,14 +11,14 @@ makeCacheMatrix <- function(x = matrix()) {
   getinverse <- function() {inv}
   
   #list of functions can be called from makeCacheMatrix$
-  list(setm=setmatrix, getm=getmatrix, seti=setinverse, geti=getinverse)
+  list(setm=setmatrix, getm=getmatrix, seti=setinverse, getinverse=getinverse)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  inv <- x$geti()
+  inv <- x$getinverse()
   if(!is.null(inv)) {
     message("return cached data.")
     return(inv)
